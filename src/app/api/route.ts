e618a7d58981b5a1f6cdc1663add69d7d1137475
Arthur Aktamirov <arthuraktamirov@gmail.com>
2024-09-24 21:06:21 +0200

    version update

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   if (req.method === "GET") {
     // Handle GET request here
-    return NextResponse.json({ app_version: "2.0.6" });
+    return NextResponse.json({ app_version: "2.0.7" });
   } else {
     return NextResponse.error();
   }
