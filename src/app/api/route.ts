26c09c2eb34ca2908ea2e7e6df026a2280003117
zaroc <arthuraktamirov@gmail.com>
2024-11-21 13:10:24 +0100

    update new version code for flutter app

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request) {
   if (req.method === "GET") {
     // Handle GET request here
-    return NextResponse.json({ app_version: "2.0.7" });
+    return NextResponse.json({ app_version: "2.1.0" });
   } else {
     return NextResponse.error();
   }
